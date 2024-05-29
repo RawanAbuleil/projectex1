dbe9384f8e24c00198266945e2146df5ccbf2c01
RawanAbuleil <rawanwaelab@icloud.com>
2024-05-29 19:55:26 +0300

    BugID:111:2024-05-29 19:55:26:BR:Rawan:10:change to capital:Add new COmmit

--- commit.sh
+++ commit.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
-
-# Check if the correct number of arguments is provided
+#checking the arguments
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <CSV file path> [additional description]"
     exit 1
 fi
 
-# Assign arguments to variables
 csv_file=$1
 additional_description=$2
 
-# Read the CSV file and extract the necessary details
 tail -n +2 "$csv_file" | while IFS=, read -r bug_id description branch_name developer priority github_url
 do
-    # Get the current date and time
     current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 
-    # Format the commit message without the GITHUB_URL
+    #  the commit message formatt
     if [ -n "$additional_description" ]; then
         commit_message="BugID:$bug_id:$current_datetime:$branch_name:$developer:$priority:$description:$additional_description"
     else
         commit_message="BugID:$bug_id:$current_datetime:$branch_name:$developer:$priority:$description"
     fi
 
-    # Check if the branch exists
+    # checks if da branch exists
     if git show-ref --quiet refs/heads/"$branch_name"; then
         git checkout "$branch_name"
     else
-        # Create the branch if it doesn't exist
+        # create da branch if  doesn't exist
         git checkout -b "$branch_name"
     fi
 
-    # Perform Git operations
     git add .
-    git commit -m "$commit_message"
     
-    # Ensure that the file containing secrets is not added to the commit
     git reset HEAD "$csv_file"
     
-    git push origin "$branch_name"
+    git commit -m "$commit_message"
+    
 
 done
